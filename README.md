5c78174a2ed74ecad97cb71d455e60da2d556bd1
Zubrungus <58639394+Zubrungus@users.noreply.github.com>
2025-01-20 21:47:08 -0600

    Add license badge to output, update README.md

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# readme-generator
-Course module 7 challenge
+# README Generator
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+## Table of contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Questions](#questions)
+## Description
+A node.js application that assists with creating a README for a repository
+## Installation
+Clone this repository to your local machine 
+## Usage
+Navigate to the cloned repository and run "node index" to begin the process. Simply follow the prompts in order to generate your repository's README.
+## License
+This software is covered under the GNU GPL V3 license
+## Questions
+- My Github profile is https://github.com/zubrungus
+- I can be reached at nickbappe@gmail.com
--- index.js
+++ index.js
@@ -14,6 +14,11 @@ inquirer.prompt([
     },
     {
         type: 'input',
+        message: 'Enter installation instructions',
+        name: 'install'
+    },
+    {
+        type: 'input',
         message: 'Enter usage instructions',
         name: 'usage'
     },
@@ -31,7 +36,7 @@ inquirer.prompt([
     {
         type: 'input',
         message: 'Enter test instructions',
-        name: 'guidelines'
+        name: 'test'
     },
     {
         type: 'input',
@@ -45,5 +50,13 @@ inquirer.prompt([
     },
 ])
 .then((response) => {
-    fs.writeFile('output/README.md', `# ${response.title}\n## Table of contents\n- [Description](#Description)\n- [Installation](#Installation)\n- [Usage](#Usage)\n- [License](#License)\n- [Contribution guidelines](#Contribution-guidelines)\n- [Test instructions](#Test-instructions)\n- [Questions](#Questions)\n`, (err) => {err ? console.error(err) : console.log('README created');});
+    let licenseBadge = '';
+    switch (response.license){
+        case 'MIT': licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'; break;
+        case 'GNU GPL V3': licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'; break;
+        case 'Apache 2.0': licenseBadge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'; break;
+
+    }    
+
+    fs.writeFile('output/README.md', `# ${response.title}\n${licenseBadge}\n## Table of contents\n- [Description](#description)\n- [Installation](#installation)\n- [Usage](#usage)\n- [License](#license)\n- [Contribution guidelines](#contribution-guidelines)\n- [Test instructions](#test-instructions)\n- [Questions](#questions)\n## Description\n${response.description}\n## Installation\n${response.install}\n## Usage\n${response.usage}\n## License\nThis software is covered under the ${response.license} license\n## Contribution guidelines\n${response.guidelines}\n## Test instructions\n${response.test}\n## Questions\n- My Github profile is https://github.com/${response.username}\n- I can be reached at ${response.email}\n`, (err) => {err ? console.error(err) : console.log('README created');});
 });
